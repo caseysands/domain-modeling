26cf473b5fc35b661b60e7c66a74636e5bbbe4d3
caseysands <casey.sands@kellogg.northwestern.edu>
2022-04-14 02:17:29 +0000

    labs

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,48 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS salespeople;
+
+CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    phone_number TEXT,
+    company_id INTEGER
+);
+
+CREATE TABLE activities(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    calls_emails TEXT,
+    date_time TEXT,
+    notes TEXT,
+    contact_id INTEGER,
+    salesperson_id INTEGER
+);
+
+CREATE TABLE companies(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE salespeople(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT
+);
+
+CREATE TABLE industries(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    industry_name TEXT,
+    company_id INTEGER,
+    salesperson_id INTEGER,
+    activity_id INTEGER,
+    contact_id INTEGER
+);
+
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,34 @@
 
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS followers;
+
+CREATE TABLE posts(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    caption TEXT,
+    user_id INTEGER,
+    time TEXT,
+    likes INTEGER
+);
+
+CREATE TABLE activities(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    like BOOLEAN,
+    post_id INTEGER,
+    user_id INTEGER,
+    comment TEXT
+);
+
+CREATE TABLE users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    screen_name TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    location TEXT
+);
+
+CREATE TABLE followers(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER
+);
